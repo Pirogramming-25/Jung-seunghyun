7fc129d4d33ed72d8a910032655a77ead64efb71
seunngh <jungshyun0615@gmail.com>
2026-07-02 00:35:23 +0900

    feat: 영화 리뷰 CRUD 및 장르 선택 기능 구현

---
+++ myMovieReviews/reviews/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'reviews'
+
+urlpatterns = [
+    path('', views.review_list, name='review_list'),
+    path('create/', views.review_create, name='review_create'),
+    path('<int:pk>/', views.review_detail, name='review_detail'),
+    path('<int:pk>/edit/', views.review_update, name='review_update'),    
+    path('<int:pk>/delete/', views.review_delete, name='review_delete'),
+]
